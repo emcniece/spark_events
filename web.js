5d58409f7c5e9155985c9b24167131c903e7e14d
Eric McNiece <emcniece@gmail.com>
2014-05-16 15:13:54 -0700

    Working version of ES Server

--- web.js
+++ web.js
@@ -1,50 +1,90 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+	// web.js
+	var express = require("express");
+	var logfmt = require("logfmt");
+	var app = express();
 
-var EventSource = require('eventsource');
-var esInitDict = {rejectUnauthorized: false};
+	var EventSource = require('eventsource');
+	var esInitDict = {rejectUnauthorized: false};
 
-var deviceID = process.env.deviceID;
-var accessToken = process.env.accessToken;
-
-// Spark URL - must use es.addEventListener and specify the event name
-var url = "https://api.spark.io/v1/devices/"+deviceID+"/events/?access_token="+accessToken;
-
-// Test URL - uses es.onmessage to capture events
-//var url = 'https://demo-eventsource.rhcloud.com/';
-
-/*===================================
-=            EventSource            =
-===================================*/
-var es = new EventSource(url);
-
-es.addEventListener('Uptime', function(e){
-	console.log( 'listener: ', JSON.parse(e.data) );
-}, false);
+	// ES Server vars
+	var http = require('http')
+	  , fs   = require('fs')
+	  , PORT = process.argv[2] || 8080
+	  , HOST = process.argv[3] || '127.0.0.1';
 
 
-es.onmessage = function(e){
-  console.log( 'onmessage: ', e.data);
-};
+	var deviceID = process.env.deviceID;	// string, your device ID
+	var accessToken = process.env.accessToken; // string, your access token
 
-es.onerror = function(){
-  console.log('ES Error');
-};
+	// Spark URL - must use es.addEventListener and specify the event name
+	var url = "https://api.spark.io/v1/devices/"+deviceID+"/events/?access_token="+accessToken;
 
+	// Test URL - uses es.onmessage to capture events
+	//var url = 'https://demo-eventsource.rhcloud.com/';
 
-/*================================
-=            Core App            =
-================================*/
+	/*===================================
+	=            EventSource            =
+	===================================*/
+	var es = new EventSource(url);
 
-app.use(logfmt.requestLogger());
+	// Only fires for Spark URL
+	es.addEventListener('Uptime', function(e){
+		console.log( 'listener: ', JSON.parse(e.data) );
+	}, false);
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+	// Only fires for Test URL
+	es.onmessage = function(e){
+	  console.log( 'onmessage: ', e.data);
+	};
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+	es.onerror = function(){
+	  console.log('ES Error');
+	};
+
+	/*================================
+	=            Core App            =
+	================================*/
+
+	app.use(logfmt.requestLogger());
+
+	app.get('/', function(req, res) {
+	  res.send('Hello World!');
+	});
+
+	var port = Number(process.env.PORT || 5000);
+	app.listen(port, function() {
+	  console.log("Listening on " + port);
+	});
+
+	/*=================================
+	=            ES Server            =
+	=================================*/ 
+ 
+	http.createServer(function (req, res) {
+	   if (req.url == '/') {
+	      res.writeHead(200, { 'Content-Type'  : 'text/event-stream'
+	                         , 'Cache-Control' : 'no-cache'
+	                         , 'Connection'    : 'keep-alive'
+	                         , "Access-Control-Allow-Origin": "*"
+	                         });
+	      console.log('Client connect');
+	 
+	      var t = setInterval(function () {
+	         console.log('Send data');
+	         res.write('data: DATA\n\n');
+	      }, 5000);
+	 
+	      res.socket.on('close', function () {
+	         console.log('Client leave');
+	         clearInterval(t);
+	      });
+	 
+	   } else {
+	      res.writeHead(200, {'Content-Type': 'text/html'});
+	      res.write(fs.readFileSync(__dirname + '/templates/index.html'));
+	      res.end()
+	   }
+	}).listen(PORT, HOST);
+		
+		
+	
