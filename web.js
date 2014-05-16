13005f47a09fa61219a9c5ac5d29f0d8498524bf
Eric McNiece <emcniece@gmail.com>
2014-05-16 14:03:19 -0700

    testing envs

--- web.js
+++ web.js
@@ -6,8 +6,8 @@ var app = express();
 var EventSource = require('eventsource');
 var esInitDict = {rejectUnauthorized: false};
 
-var deviceID = "50ff70065067545642080387";
-var accessToken = "b83d8349a10a34df67f74d4afed7783851534819";
+var deviceID = process.env.deviceID;
+var accessToken = process.env.accessToken;
 
 // Spark URL - must use es.addEventListener and specify the event name
 var url = "https://api.spark.io/v1/devices/"+deviceID+"/events/?access_token="+accessToken;
@@ -46,6 +46,5 @@ app.get('/', function(req, res) {
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-	console.log( process.env);
   console.log("Listening on " + port);
 });
